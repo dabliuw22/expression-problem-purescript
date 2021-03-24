fb4801c1200c30fe46d0749dd9eaa4312bd176c6
Will Leyton <wleylop22@gmail.com>
2021-03-24 07:46:38 -0500

    Add Problem

---
+++ src/Problem.purs
@@ -0,0 +1,17 @@
+module Problem where
+
+import Prelude
+
+data Expr
+  = Val Int
+  | Add Expr Expr
+
+eval :: Expr -> Int
+eval (Val i) = i
+
+eval (Add x y) = eval x + eval y
+
+myExpr :: Expr
+myExpr = Add (Val 1) (Add (Val 10) (Val 2))
+
+-- Problem: If we add new types of Expr, we must update all our functions.
